fe361b07fb54b2880e4090b9c91f499746eaa90c
Juan-luke Klopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-02-09 11:08:26 +0000

    made code more efficient by refactoring it.

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,6 @@ const App = () => {
   const contractAddress = "0xd94811E9931eC443f9C93BC421c236c08B57CFD7";
   const contractABI = abi.abi;
 
- /* const CheckIfWalletIsConnected = async () => {
-    try {
-      const { ethereum } = window;
-
-    if (!ethereum){
-     console.log("Please ensure that you  have Metamask istalled.");
-    } else {
-      console.log("We have the ethereum object ", ethereum);
-      getAllWaves();
-      }
-
-    const accounts = await ethereum.request({method: "eth_accounts"});
-
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("found an autherized account: ", account);
-      setCurrentAccount(account);
-    } else {
-      console.log("No autherized account found.");
-    }
-    } catch (error) {
-      console.log(error)
-    }
- }  */
-
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -118,36 +93,7 @@ const App = () => {
   } catch (error) {
     console.log(error);
   }
-};
-  /*const getAllWaves = async () => {
-    try {
-      const { ethereum } = window;
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-        const waves = await wavePortalContract.getAllWaves();
-
-
-        let wavesCleaned = [];
-        waves.forEach(wave => {
-          wavesCleaned.push({
-            address: wave.waver,
-            timestamp: new Date(wave.timestamp * 1000),
-            message: wave.message
-          });
-          console.log(wave);
-        });
-
-        setAllWaves(wavesCleaned);
-      } else {
-        console.log("Ethereum object doesn't exist!")
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }*/
+  };
 
   useEffect(() => {
   let wavePortalContract;
